2e852cfe9313286f5a0377c1d4dca9080dc4a945
Alec Ellsworth <alec.ells123@gmail.com>
2025-01-18 10:54:19 -0600

    favicon.io 404?

--- chainstory/chainstory/settings.py
+++ chainstory/chainstory/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'linkapp',
+    'channels',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'linkapp'
 ]
+
+ASGI_APPLICATION = 'chainstory.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
