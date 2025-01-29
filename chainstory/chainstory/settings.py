8fd8ab2ef2b9377569526ac86bc94a5047513ace
Alec Ellsworth <alec.ells123@gmail.com>
2025-01-29 12:43:08 -0600

    Ugly but it works!

--- chainstory/chainstory/settings.py
+++ chainstory/chainstory/settings.py
@@ -53,6 +53,7 @@ ASGI_APPLICATION = 'chainstory.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'linkapp/static'),
+    BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
